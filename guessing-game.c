6097a554f43c04ca2a362f949636d2fb6e1b24ee
Igor Moura <igor.moura254@hotmail.com>
2021-11-15 10:45:23 -0300

    printing attempts

--- guessing-game.c
+++ guessing-game.c
@@ -4,4 +4,14 @@ int main(){
     printf("************************************\n");
     printf("* Welcome to the guessing game *\n");
     printf("************************************\n");
+
+    int secretNumber = 42;
+
+    int attempt;
+
+    printf("What is your attempt? ");
+
+    scanf("%d", &attempt);
+
+    printf("Your attempt was %d", attempt);
 }
