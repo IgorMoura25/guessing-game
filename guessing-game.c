b4eb0b62aa206a9dce6a181cec8464bfe5f0194d
Igor Moura <igor.moura254@hotmail.com>
2021-11-21 19:02:31 -0300

    Added some ascii art

--- guessing-game.c
+++ guessing-game.c
@@ -1,17 +1,36 @@
 #include <stdio.h>
 
 int main(){
-    printf("************************************\n");
-    printf("* Welcome to the guessing game *\n");
-    printf("************************************\n");
+    printf("\n\n");
+    printf("          P  /_\\  P                              \n");
+    printf("         /_\\_|_|_/_\\                            \n");
+    printf("     n_n | ||. .|| | n_n         Welcome to the     \n");
+    printf("     |_|_|nnnn nnnn|_|_|     Guessing Game! \n");
+    printf("    |\" \"  |  |_|  |\"  \" |                     \n");
+    printf("    |_____| ' _ ' |_____|                         \n");
+    printf("          \\__|_|__/                              \n");
+    printf("\n\n");
 
     int secretNumber = 42;
 
-    int attempt;
+    int guess;
 
-    printf("What is your attempt? ");
+    for(int i = 1; i <= 3; i++){
+        printf("Chance %d of %d \n", i, 3);
+        printf("What is your guess? ");
 
-    scanf("%d", &attempt);
+        scanf("%d", &guess);
 
-    printf("Your attempt was %d", attempt);
+        printf("Your guess was %d \n", guess);
+
+        int isCorrect = guess == secretNumber;
+
+        if(isCorrect){
+            printf("Congratulations! Your guess was right :)");
+            break;
+        }
+        else{
+            printf("Your guess is wrong :(\n");
+        }
+    }
 }
